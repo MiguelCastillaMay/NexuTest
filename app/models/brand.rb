a31016b598c7349ba183e4b0e3607a96be9898b8
Miguel Castilla May <miguelcastilla50@gmail.com>
2024-04-09 16:06:42 -0600

    update the average price of the brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,4 +2,9 @@ class Brand < ApplicationRecord
   has_many :models
   validates :name, presence: true, uniqueness: true
   # validates_numericality_of :average_price, only_integer: true, greater_than_or_equal_to: 100000
+
+  def update_average_price
+    average_price = models.average(:average_price)
+    update(average_price: average_price)
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -2,4 +2,10 @@ class Model < ApplicationRecord
   belongs_to :brand
   validates :name, presence: true,  uniqueness: true
   validates :average_price, numericality: { only_integer: true, greater_than_or_equal_to: 100000 }, allow_blank: true
+
+  after_save :update_brand_average_price
+
+  def update_brand_average_price
+    brand.update_average_price
+  end
 end
